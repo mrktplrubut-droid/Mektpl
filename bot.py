51d3eb1a30bf23743e49b266c9d3734a0217686d
mrktplrubut-droid <mrktplrubut@gmail.com>
2026-08-15 00:41:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,6 +76,7 @@ from handlers.edit_price import router as edit_price_router
 from handlers.ewallet import router as ewallet_router
 
 from handlers.favorite import router as favorite_router
+from handlers.rating import router as rating_router
 
 from handlers.pay import router as pay_router
 from handlers.cancel import router as cancel_router
@@ -139,6 +140,7 @@ dp.include_router(withdraw_confirm_router)
 dp.include_router(admin_router)
 
 dp.include_router(favorite_router)
+dp.include_router(rating_router)
 
 # MARKETPLACE
 dp.include_router(marketplace_router)
