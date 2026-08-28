bb7a968a06ec78a31b6f6310081f91dc4b1813f1
mrktplrubut-droid <mrktplrubut@gmail.com>
2026-08-29 02:10:49 +0700

    Add files via upload

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -29,9 +29,11 @@ def home_kb(user_id: int):
             ],
             [
                 InlineKeyboardButton(
-                    text="🎨 Kreator",
-                    callback_data="creator"
-                ),
+                    text="💎 VIP / Kreator",
+                    callback_data="premium"
+                )
+            ],
+            [
                 InlineKeyboardButton(
                     text="📂 Menu Lainnya",
                     callback_data="menu_lainnya"
